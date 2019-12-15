c7f2fb0f4162cff515285808916b4ae61bd847b0
Muradq <muradgaray@gmail.com>
2019-12-16 00:30:53 +0400

    edited gitignore file

--- .venv/bin/activate.fish
+++ .venv/bin/activate.fish
@@ -29,7 +29,7 @@ end
 # unset irrelevant variables
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/home/it-pc-003/PycharmProjects/btr_project/.venv"
+set -gx VIRTUAL_ENV "/home/m/PycharmProjects/emlak/.venv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
--- pagesapp/settings.py
+++ pagesapp/settings.py
@@ -78,11 +78,8 @@ WSGI_APPLICATION = 'pagesapp.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'btredb',
-        'USER': 'postgres',
-        'PASSWORD': '@linuxmurad1992',
-        'HOST': 'localhost'
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
